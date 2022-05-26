ba3bac7e0da207f3c0a0284a1c833e4c27cb4bb4
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-05-26 09:18:52 +0000

    Added getInfo function - variables at script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Function to Get Information (including Invalid Input)
+function getInfo() {
+    // Set Up Variable
+    let txt = "";
+    // Get the Values of the Input Fields
+    let num1 = document.getElementById("myyear1").value;
+    let num2 = document.getElementById("myyear2").value;
+}
