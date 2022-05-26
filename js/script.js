e17d795ffa928a9d036b5167cfb0f593fae3e6e3
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-05-26 09:24:03 +0000

    Added condition for invalid input at script.js

--- js/script.js
+++ js/script.js
@@ -5,4 +5,8 @@ function getInfo() {
     // Get the Values of the Input Fields
     let num1 = document.getElementById("myyear1").value;
     let num2 = document.getElementById("myyear2").value;
+    // Check if inputs are valid
+    if (isNaN(num1) || isNaN(num2) || num1.toString().length == 0 || num2.toString().length == 0 || num1<1901 || num1>2099 || num2<1902 || num2>2100 || !Number.isInteger(Number(num1)) || !Number.isInteger(Number(num2)) || num2<num1) {
+        
+    }
 }
