ee213aa8c32cf22eeaa3a0929536e78aa0500fb1
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-05-26 09:33:56 +0000

    Added conditions for out of range numbers at script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,28 @@ function getInfo() {
     let num1 = document.getElementById("myyear1").value;
     let num2 = document.getElementById("myyear2").value;
     // Check if inputs are valid
-    if (isNaN(num1) || isNaN(num2) || num1.toString().length == 0 || num2.toString().length == 0 || num1<1901 || num1>2099 || num2<1902 || num2>2100 || !Number.isInteger(Number(num1)) || !Number.isInteger(Number(num2)) || num2<num1) {
-        
+    if (isNaN(num1) || isNaN(num2) || num1.toString().length == 0 || num2.toString().length == 0 || num1<1900 || num1>2099 || num2<1901 || num2>2100 || !Number.isInteger(Number(num1)) || !Number.isInteger(Number(num2)) || num2<num1) {
+
+        // Set Condition for Not a Number
+        if (isNaN(num1) || isNaN(num2)) {
+            // Condition if first input is not a number
+            if (isNaN(num1)) txt += `Invalid Input.  The first year must be a number.`;
+            // Condition if second input is not a number
+            if (isNaN(num2)) txt += `Invalid Input.  The second year must be a number.`;
+        }
+
+        // Set Condition for Empty Inputs
+        if (num1.toString().length==0 || num2.toString().length==0) {
+            // Condition if first input is empty
+            if (num1.toString().length==0) txt += `Invalid Input.  First Year not entered.`;
+            // Condition if second input is empty
+            if (num2.toString().length==0) txt += `Invalid Input.  Second Year not entered.`
+        }
+
+        // Condition if First Year is not in range
+        if (num1<1900 || num1>2099) txt += `Invalid Input.  First Year must be between 1900 and 2099.`;
+
+        // Condition if Second Year is not in range
+        if (num2<1901 || num2>2100) txt += `Invalid Input.  Second Year must be between 1901 and 2100.`;
     }
 }
