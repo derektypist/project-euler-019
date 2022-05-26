e93493ba36e892bd8117886a2c2da749cde54768
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-05-26 10:33:32 +0000

    Added daysInMonth function at script.js

--- js/script.js
+++ js/script.js
@@ -64,6 +64,21 @@ function countingSundays(firstYear,lastYear) {
     }
 
     function daysInMonth(month,year) {
-        
+        switch (month) {
+            case 0: return 31;
+            case 1: return isLeapYear(year) ? 29 : 28;
+            case 2: return 31;
+            case 3: return 30;
+            case 4: return 31;
+            case 5: return 30;
+            case 6: return 31;
+            case 7: return 31;
+            case 8: return 30;
+            case 9: return 31;
+            case 10: return 30;
+            case 11: return 31;
+        }
     }
+
+    
 }
